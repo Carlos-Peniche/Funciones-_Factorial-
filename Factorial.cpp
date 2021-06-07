b5d6eada882bb00ac3c807ea6579b31e363c9b56
Carlos Peniche <serp11345@gmail.com>
2021-06-07 13:37:11 -0500

    V Final

--- Factorial.cpp
+++ Factorial.cpp
@@ -3,30 +3,26 @@
 double factorial(double x);
 
 int main(){
-	
+
 	double numero, calculo;
 	int salir;
-	
+
 	do{
 	
-		// Ingreso del dato para realizar el calculo 
-		
+// Ingreso del dato para realizar el calculo 	
 		printf("\n\nIngrese un numero: ");
 		scanf("%lf", &numero);
-		
-		
-		//Si el número es mayor a 0, se hará el cálculo del factorial. 
+
+		//Condicion de ejecucion 
+	
+	
 		if(numero > 0){
-		
-			
+
 			calculo = factorial (numero);
 			
-			printf("El factorial del numero %.0lf es de: %.0lf ", numero, calculo);
-			
-			
+			printf("El factorial del numero %.0lf es de: %.0lf ", numero, calculo);	
 		}
-		
-	//validacion de un dato positivo 
+//validacion de un dato positivo 
 		else{
 			
 			do{
@@ -38,35 +34,30 @@ int main(){
 			
 				calculo = factorial (numero);
 				printf("El factorial del numero %.0lf es de: %.0lf ", numero, calculo);
-				
-			}while(numero < 0);	//Aqui hay un pequeño error, si volvemos a ingresar un número negativo, se rompe el programa.
-						
+			}while(numero < 0);	
 		}
+
 				
-			
 			printf("\nDesea realizar otro calculo \n [Si=1]\n [No=0]\n : ");
 			scanf("%d", &salir);
 		
 	}while(salir != 0); 
 		
-//Termino del programa 
-		
+//Termino del programa 	
 	printf("\n Gracias por usar el programa :)\n");
-	
+
 	return 0;
 }
 
 double factorial(double x){
-	
-	if(x == 0 || x == 1){
-		
+	if(x == 0 || x == 1){	
 		return 1;
 	}
-	
+
 	else{
-		
+
 		return x * factorial (x - 1);
 	}
-	
+
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Funciones-_Factorial-
-Tarea
+
+Tarea / Ejercicio 
+
+Hola profesor, espero se encuentre bien.
+Este esel reposiorio en GitHub donde puede encontrar el codigo fuente del ejercicio :
+
+Gracias por su atencion Profesor excelente inicio de semana.
+
+Atte: Carlos Eduardo Peniche Lopez 
